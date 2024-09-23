89368b22f20e7a1d9852912e837db4d714af7dc6
atefm <atef.mannai@esprit.tn>
2024-09-23 19:36:30 +0100

    first commit

---
+++ src/main/java/tn/esprit/foyeresprit3/FoyerEsprit3Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.foyeresprit3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoyerEsprit3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoyerEsprit3Application.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/foyeresprit3/FoyerEsprit3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.foyeresprit3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoyerEsprit3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
